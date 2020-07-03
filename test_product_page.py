165b1c55b32d0b39501c5ce545b11b1da8fa6f4d
Artem.Nesterenko <artem.nesterenko@magenta-technology.com>
2020-07-03 12:45:22 +0400

    delete unnecessary comments in code

--- test_product_page.py
+++ test_product_page.py
@@ -2,8 +2,6 @@
 
 import pytest
 import time
-from pages.base_page import BasePage
-from pages.main_page import MainPage
 from pages.basket_page import BasketPage
 from pages.login_page import LoginPage
 from pages.product_page import ProductPage
@@ -15,7 +13,7 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?prom
 class TestUserAddToBasketFromProductPage():
     @pytest.fixture(scope="function", autouse=True)
     def setup(self, browser):
-        page = ProductPage(browser, link)          # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+        page = ProductPage(browser, link)       # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
         page.open()                             # открываем страницу
         page.go_to_login_page()                 # выполняем метод страницы - переходим на страницу логина
         login_page = LoginPage(browser, browser.current_url)
@@ -28,56 +26,56 @@ class TestUserAddToBasketFromProductPage():
 
 
     def test_user_cant_see_success_message(self, browser):
-        page = ProductPage(browser, link)          # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-        page.open()                             # открываем страницу
-        page.should_not_be_success_message()    # проверяем что нет собщения об успехе
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_not_be_success_message()
 
 
     @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
-        page = ProductPage(browser, link)          # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-        page.open()                             # открываем страницу
-        page.add_product_to_basket()            # выполняем метод страницы - добавляем товар в корзину и проверяем успешность добавления
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_product_to_basket()
 
 
 @pytest.mark.need_review
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-                                  #"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-                                  #"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-                                  #"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-                                  #"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-                                  #"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
                                   pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
-                                  #"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"])
 def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
-    page.open()                             # открываем страницу
-    page.add_product_to_basket()            # выполняем метод страницы - добавляем товар в корзину и проверяем успешность добавления
+    page.open()
+    page.add_product_to_basket()
 
 
 @pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
-    page = ProductPage(browser, link)          # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                             # открываем страницу
-    page.add_product_to_basket()            # выполняем метод страницы - добавляем товар в корзину и проверяем успешность добавления
-    page.should_not_be_success_message()    # проверяем что нет собщения об успехе
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket()
+    page.should_not_be_success_message()
 
 
 def test_guest_cant_see_success_message(browser):
-    page = ProductPage(browser, link)          # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                             # открываем страницу
-    page.should_not_be_success_message()    # проверяем что нет собщения об успехе
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
 
 
 @pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser):
-    page = ProductPage(browser, link)          # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                             # открываем страницу
-    page.add_product_to_basket()            # выполняем метод страницы - добавляем товар в корзину и проверяем успешность добавления
-    page.success_message_should_disappeared()    # проверяем что нет собщения об успехе
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket()
+    page.success_message_should_disappeared()
 
 
 def test_guest_should_see_login_link_on_product_page(browser):
@@ -98,8 +96,8 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
 
 @pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
-    page = ProductPage(browser, link)          # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                             # открываем страницу
-    page.go_to_login_page()                 # выполняем метод страницы - переходим на страницу логина
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
